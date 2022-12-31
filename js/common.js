9f9bf632c0d28a2404e6ce4ce56449ebbc225eb1
minoriyukida <rinngo903@outlook.jp>
2022-12-31 23:25:34 +0900

    実績紹介

--- js/common.js
+++ js/common.js
@@ -30,3 +30,24 @@ window.onload = function () {
     pauseOnDotsHover: false,
     consolelog:"aaa",
 });
+
+// モーダル
+var access = $.cookie('access')
+if(!access){
+  flag = true;
+  $.cookie('access', false);
+}else{
+  flag = false	
+}
+
+//モーダル表示
+$(".modal-open").modaal({
+start_open:flag,
+overlay_close:true,
+before_open:function(){
+  $('html').css('overflow-y','hidden');
+},
+after_close:function(){
+  $('html').css('overflow-y','scroll');
+}
+});
