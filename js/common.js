8fde832d5a7fddb04f745cb54535dd50f093e971
minoriyukida <rinngo903@outlook.jp>
2022-12-31 11:24:34 +0900

    コメント整理

--- js/common.js
+++ js/common.js
@@ -14,19 +14,19 @@ window.onload = function () {
 
 //   スライドショー
   $('.slider').slick({
-    fade:true,//切り替えをフェードで行う。初期値はfalse。
-    autoplay: false,//自動的に動き出すか。初期値はfalse。
-    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-    speed:1000,//スライドの動きのスピード。初期値は300。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 1,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    arrows: true,//左右の矢印あり
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    dots: true,//下部ドットナビゲーションの表示
-    pauseOnFocus: false,//フォーカスで一時停止を無効
-    pauseOnHover: false,//マウスホバーで一時停止を無効
-    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+    fade:true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    speed:1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
     consolelog:"aaa",
 });
