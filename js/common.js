d7fb21daf8f2f6b89d92e1cf5174f02521ac4093
minoriyukida <rinngo903@outlook.jp>
2023-01-11 00:34:57 +0900

    fix

--- js/common.js
+++ js/common.js
@@ -31,47 +31,37 @@ window.onload = function () {
     consolelog:"aaa",
 });
 
-// モーダル
-var access = $.cookie('access')
-if(!access){
-  flag = true;
-  $.cookie('access', false);
-}else{
-  flag = false	
-}
-
 //モーダル表示
-// $(".modal-open").modaal({
-// start_open:flag,
-// overlay_close:true,
-// before_open:function(){
-//   $('html').css('overflow-y','hidden');
-// },
-// after_close:function(){
-//   $('html').css('overflow-y','scroll');
-// }
-// });
-
-// アコーディオン
-$('.skill-title').on('click', function() {//タイトル要素をクリックしたら
-	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
-	$(findElm).slideToggle();//アコーディオンの上下動作
-    
-	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-		$(this).removeClass('close');//クラス名を除去し
-	}else{//それ以外は
-		$(this).addClass('close');//クラス名closeを付与
+$(".modaal").modaal({
+	overlay_close:true,
+	before_open:function(){
+		$('html').css('overflow-y','hidden');
+	},
+	after_close:function(){
+		$('html').css('overflow-y','scroll');
 	}
 });
 
-//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+// アコーディオン
+$('.skill-title').on('click', function() {
+	var findElm = $(this).next(".box");
+	$(findElm).slideToggle();
+    
+	if($(this).hasClass('close')){
+		$(this).removeClass('close');
+	}else{//それ以外は
+		$(this).addClass('close');
+	}
+});
+
+
 $(window).on('load', function(){
-	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
-	$(".open").each(function(index, element){	//openクラスを取得
-		var Title =$(element).children('.skill-title');	//openクラスの子要素のtitleクラスを取得
-		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
-		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
-		$(Box).slideDown(500);					//アコーディオンを開く
+	$('.accordion-area li:first-of-type section').addClass("open");
+	$(".open").each(function(index, element){	
+		var Title =$(element).children('.skill-title');	
+		$(Title).addClass('close');
+		var Box =$(element).children('.box');
+		$(Box).slideDown(500);
 	});
 });
 
@@ -79,61 +69,67 @@ $(window).on('load', function(){
 function GethashID (hashIDName){
 	if(hashIDName){
 		//タブ設定
-		$('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
-			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-				$('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-				//表示させるエリア設定
-				$(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-				$(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
+		$('.tab li').find('a').each(function() {
+			var idName = $(this).attr('href');	
+			if(idName == hashIDName){
+				var parentElm = $(this).parent();
+				$('.tab li').removeClass("active");
+				$(parentElm).addClass("active");
+				$(".area").removeClass("is-active");
+				$(hashIDName).addClass("is-active");	
 			}
 		});
 	}
 }
 
 $('.tab a').on('click', function() {
-	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
-	GethashID (idName);//設定したタブの読み込みと
-	return false;//aタグを無効にする
+	var idName = $(this).attr('href');	
+	GethashID (idName);
+	return false;
 });
 
 
 $(window).on('load', function () {
-    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
-    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
-	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
-	GethashID (hashName);//設定したタブの読み込み
+    $('.tab li:first-of-type').addClass("active");
+    $('.area:first-of-type').addClass("is-active");
+	var hashName = location.hash;
+	GethashID (hashName);
 });
 
 // 画面ローディング
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-	duration: 1000,//時間指定(1000＝1秒)
-	strokeWidth: 0.2,//進捗ゲージの太さ
-	color: '#555',//進捗ゲージのカラー
-	trailWidth: 0.2,//ゲージベースの線の太さ
-	trailColor: '#bbb',//ゲージベースの線のカラー
-	text: {//テキストの形状を直接指定				
-		style: {//天地中央に配置
+var bar = new ProgressBar.Line(splash_text, {
+	easing: 'easeInOut',
+	duration: 1000,
+	strokeWidth: 0.2,
+	color: '#555',
+	trailWidth: 0.2,
+	trailColor: '#bbb',
+	text: {				
+		style: {
 			position: 'absolute',
 			left: '50%',
 			top: '50%',
 			padding: '0',
-			margin: '-30px 0 0 0',//バーより上に配置
+			margin: '-30px 0 0 0',
 			transform:'translate(-50%,-50%)',
 			'font-size':'1rem',
 			color: '#fff',
 		},
-		autoStyleContainer: false //自動付与のスタイルを切る
+		autoStyleContainer: false 
 	},
 	step: function(state, bar) {
-		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+		bar.setText(Math.round(bar.value() * 100) + ' %'); 
 	}
 });
 
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
-});  
+bar.animate(1.0, function () {
+	$("#splash").delay(500).fadeOut(800);
+});
+
+// ページトップリンク
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
