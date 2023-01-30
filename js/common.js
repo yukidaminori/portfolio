933e89ee0e69777c8bc1159d76230d44f285f29b
minoriyukida <rinngo903@outlook.jp>
2023-01-30 23:54:02 +0900

    プロフィールの修正

--- js/common.js
+++ js/common.js
@@ -29,6 +29,35 @@ window.onload = function () {
     pauseOnDotsHover: false,
 });
 
+// フェードイン
+function fadeAnime(){
+	$('.fadeUpTrigger').each(function(){
+		var elemPos = $(this).offset().top-50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+			$(this).addClass('fadeUp');
+		}else{
+			$(this).removeClass('fadeUp');
+		}
+	});
+	
+	$('.fadeDownTrigger').each(function(){
+		var elemPos = $(this).offset().top-50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+			$(this).addClass('fadeDown');
+		}else{
+			$(this).removeClass('fadeDown');
+		}
+	});
+}
+	
+$(window).scroll(function (){
+	fadeAnime();
+});
+
 //モーダル表示
 $(".modaal").modaal({
 	overlay_close:true,
